bf996753ed589c31c2aaa63c76de4118b3c57a1b
Israel Vidal <israelcvidal@gmail.com>
2018-03-06 10:27:56 -0300

    Modifying test file

--- Linked Lists/test.c
+++ Linked Lists/test.c
@@ -4,18 +4,41 @@
 
 int main(int argc, char const *argv[])
 {
+	printf("Creating empty list...\n");
 	LinkedList* list = create();
+
+	printf("Inserting element at head of list...\n");
 	list = insert(list, 1);
+
+	printf("Inserting element at head of list...\n");
 	list = insert(list, 2);
+
+	printf("Inserting element at head of list...\n");
 	list = insert(list, 10);
+
+	printf("printList:\n");
 	printList(list);
 
+	printf("reversePrint\n");
+	reversePrint(list);
+	printf("\n");
+
+	printf("recursivePrint:\n");
+	recursivePrint(list);
+
+	printf("recursiveRemove: 2\n");
 	list = recursiveRemove(list, 2);
 	printList(list);
 
-	list = recursiveRemove(list, 2);
+	printf("removeList:\n");
+	list = removeList(list, 10);
 	printList(list);
 
+	printf("is empty? %s\n", empty(list) ? "true": "false");
+	
+	printf("Is 1 in list? %s\n", (search(list, 1)!=NULL) ? "true": "false");
+
+	printf("freeList...\n");
 	freeList(list);
 	return 0;
 }
